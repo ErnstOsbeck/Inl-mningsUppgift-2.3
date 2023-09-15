d253b73f429a8241d51fa0245bdb88c9a7233b25
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-15 12:46:46 +0200

    Add project files.

---
+++ InlämningsUppgift 2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_2._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du hyra bilen?");
+            byte dagar = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt ska du köra?");
+            short distans = short.Parse(Console.ReadLine());
+            int pris = 300 + (dagar - 1) * 500 + distans;
+            Console.WriteLine("Det kommer kosta " + pris + " kronor att hyra denna bilen");
+            Console.Read();
+        }
+    }
+}
